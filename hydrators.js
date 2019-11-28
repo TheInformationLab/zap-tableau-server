aa5eed6a32686e98d09443f92590932b4848ad36
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2019-11-28 13:56:45 +0000

    Add download view image and preview image

--- hydrators.js
+++ hydrators.js
@@ -1,5 +1,5 @@
 const hydrators = {
-  downloadFile: (z, bundle) => {
+  downloadWorkbook: (z, bundle) => {
     // use standard auth to request the file
     const filePromise = z.request({
       url: `${bundle.authData.serverUrl}/api/${bundle.authData.apiVersion}/sites/${bundle.authData.creds.site.id}/workbooks/${bundle.inputData.workbookId}/content?includeExtract=true`,
@@ -16,6 +16,40 @@ const hydrators = {
         return url;
       });
   },
+	downloadWorkbookPreviewImage: (z, bundle) => {
+    // use standard auth to request the file
+    const filePromise = z.request({
+      url: `${bundle.authData.serverUrl}/api/${bundle.authData.apiVersion}/sites/${bundle.authData.creds.site.id}/workbooks/${bundle.inputData.workbookId}/previewImage`,
+      method: 'GET',
+      raw: true,
+      headers: {
+        'X-Tableau-Auth' : bundle.authData.creds.token
+      }
+    });
+    // and swap it for a stashed URL
+    return z.stashFile(filePromise, null, `${bundle.inputData.filename}.png`)
+      .then((url) => {
+        z.console.log(`Workbook Preview Image Stashed URL = ${url}`);
+        return url;
+      });
+  },
+	downloadViewImage: (z, bundle) => {
+    // use standard auth to request the file
+    const filePromise = z.request({
+      url: `${bundle.authData.serverUrl}/api/${bundle.authData.apiVersion}/sites/${bundle.authData.creds.site.id}/views/${bundle.inputData.viewId}/image?resolution=${bundle.inputData.resolution}&maxAge=1`,
+      method: 'GET',
+      raw: true,
+      headers: {
+        'X-Tableau-Auth' : bundle.authData.creds.token
+      }
+    });
+    // and swap it for a stashed URL
+    return z.stashFile(filePromise, null, `${bundle.inputData.filename}.png`)
+      .then((url) => {
+        z.console.log(`View Image Stashed URL = ${url}`);
+        return url;
+      });
+  }
 };
 
 module.exports = hydrators;
